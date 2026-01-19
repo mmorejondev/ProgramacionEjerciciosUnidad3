5dccd066126ca4e05a2915be174e2ab4740ccc22
mmorejondev <mmorejondev@gmail.com>
2026-01-19 14:43:33 +0100

    Ejercicio 8 empezado, no terminado

---
+++ src/com/ejud3/ej8/biblioteca/Libro.java
@@ -0,0 +1,31 @@
+package com.ejud3.ej8.biblioteca;
+
+public class Libro {
+/*
+ * titulo (String, privado): Almacena el título del libro.
+autor (String, privado): Almacena el nombre del autor del libro.
+anioPublicacion (int, privado): Almacena el año de publicación del libro.
+disponible (boolean, privado): Indica si el libro está disponible para préstamo.
+*/
+	
+	private String titulo;
+	private String autor;
+	private int anioPublicacion;
+	private boolean disponible;
+	
+	/*Constructores*/
+	public Libro(String titulo, String autor, int anioPublicacion) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.anioPublicacion = anioPublicacion;
+		this.disponible = true;
+	}
+	/*Getters y Setters*/
+	public String getTitulo() {
+		return this.titulo;
+	}
+	
+	
+	
+	
+}
