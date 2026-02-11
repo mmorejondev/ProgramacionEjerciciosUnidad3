80d754ea4a04766694e512ef17fc37602ee4e715
mmorejondev <mmorejondev@gmail.com>
2026-02-11 09:05:51 +0100

    Añadido ejercicio 14 completo

--- src/com/ejud3/ej14/Principal.java
+++ src/com/ejud3/ej14/Principal.java
@@ -4,16 +4,18 @@ public class Principal {
 
 	public static void main(String[] args) {
 
+		System.out.println("Vehículo de combustión");
 		VehiculoCombustionClase vc1 = new VehiculoCombustionClase();
 		vc1.recargarCombustible(50);
 		vc1.consumirCombustible(200);
 		
+		System.out.println("Vehículo eléctrico");
 		VehiculoElectricoClase ve1 = new VehiculoElectricoClase();
 		ve1.cargarBateria(40);
 		System.out.println(ve1.obtenerAutonomia());
 		
 		
-		
+		System.out.println("Vehículo híbrido");
 		VehiculoHibridoClase vh1 = new VehiculoHibridoClase();
 		vh1.cargarBateria(45);
 		System.out.println(vh1.obtenerAutonomia());
